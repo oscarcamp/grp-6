16c0ad5ee49da86b27ba2c4bab398a255725ff45
oscarcamp <33449861+oscarcamp@users.noreply.github.com>
2020-03-09 10:09:48 +0100

    oscar stylning fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 <h2 className="home-h2">By: group-6</h2>
             </div>
             <div className="btn-group btn-group-toggle home-btn" data-toggle="buttons">
-                <Link to="./CreateQuiz" className="btn btn-1 btn-success  btn-lg">Create Quiz</Link>
+                <Link to="./AddQuiz" className="btn btn-1 btn-success  btn-lg">Create Quiz</Link>
                 <Link to="./QuizList" className="btn btn-2 btn-primary  btn-lg">Select Quiz</Link>
             </div>
         </div>
