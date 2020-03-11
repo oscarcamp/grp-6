d562cee6e64b33cfe7f00f335da17a234b59cb8e
MyNa Do <myna.do@mahoyo.com>
2020-03-11 16:44:40 +0100

    fixed home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,13 @@ const Home = () => {
 	return(
 		<div className="homecon">
 			<div className="container-fluid">
-				<div className="home-text">
-					<h1>Quiz-app</h1>
-					<h2 className="home-h2">By: group-6</h2>
+				<div className="home-text mb-3">
+					<h1>Quiz App</h1>
+					<h2 className="home-h2">By: Group 6</h2>
 				</div>
-				<div className="btn-group btn-group-toggle start-btn" data-toggle="buttons">
-					<Link to="./AddQuiz" className="btn btn-1 btn-primary  btn-lg">Create Quiz</Link>
-					<Link to="./QuizList" className="btn btn-2 home-select  btn-lg">Select Quiz</Link>
+				<div className="btn-group btn-group-toggle start-btn mt-3" data-toggle="buttons">
+					<Link to="./AddQuiz" className="btn btn-1 btn-primary btn-lg">Create Quiz</Link>
+					<Link to="./QuizList" className="btn btn-2 home-select btn-lg">Select Quiz</Link>
 				</div>
 			
 			</div>
